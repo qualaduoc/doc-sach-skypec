81964af4464e6d6a451884a5ab6152ac514a6de6
taoladuoc <nguyenthanhduocathy@gmail.com>
2026-06-29 01:03:37 +0700

    Sửa lỗi thiếu import PendingIntent trong MainActivity.java gây lỗi biên dịch

--- android/app/src/main/java/com/skypec/reader/MainActivity.java
+++ android/app/src/main/java/com/skypec/reader/MainActivity.java
@@ -1,6 +1,7 @@
 package com.skypec.reader;
 
 import android.annotation.SuppressLint;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
